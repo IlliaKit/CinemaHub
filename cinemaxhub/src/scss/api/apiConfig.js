24cb7dfb47d221f624c2f585a998519f4ea38318
Illia Yanchenko <kitilla7@gmail.com>
2024-07-18 19:40:24 +0200

    Config API

---
+++ cinemaxhub/src/scss/api/apiConfig.js
@@ -0,0 +1,8 @@
+const apiConfig = {
+    baseUrl: 'https://api.themoviedb.org/3/',
+    apiKey: 'f909cafb2c2552aa92eaf33c60c2edde',
+    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
+}
+
+export default apiConfig;
