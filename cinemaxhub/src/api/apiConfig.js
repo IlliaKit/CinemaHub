7453ccb6f7e97aa9b3a3043626bc6cd90b870c01
Illia Yanchenko <kitilla7@gmail.com>
2024-07-20 20:13:09 +0200

    add Slider component

--- cinemaxhub/src/api/apiConfig.js
+++ cinemaxhub/src/api/apiConfig.js
@@ -3,6 +3,6 @@ const apiConfig = {
     apiKey: 'f909cafb2c2552aa92eaf33c60c2edde',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
-}
+};
 
 export default apiConfig;
