5e006172be3016dd4f2eda121eafffbf6b732238
Illia Yanchenko <kitilla7@gmail.com>
2024-07-27 15:30:19 +0200

    add movieGrid

--- cinemaxhub/src/pages/Catalog.jsx
+++ cinemaxhub/src/pages/Catalog.jsx
@@ -7,16 +7,23 @@ import PageHeader from '../components/page-header/PageHeader';
 
 import { category as cate } from '../api/tmdbApi';
 
+import MovieGrid from '../components/movie-grid/MovieGrid';
+
 const Catalog = () => {
 
   const {category} = useParams();
-
 
   return (
   <>
     <PageHeader>
       {category === cate.movie ? 'Movies' : 'TV Series'}
     </PageHeader>
+
+    <div className="container">
+        <div className="section mb-3">
+          <MovieGrid category={category}/>
+        </div>
+    </div>
   </>
   );
 }
