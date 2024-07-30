8b0114e2b8b6f37d76c3bd2d8ef3acbda478a0f1
Illia Yanchenko <kitilla7@gmail.com>
2024-07-30 20:00:00 +0200

    add Detail, castList, VideoList

--- cinemaxhub/src/config/Routes.jsx
+++ cinemaxhub/src/config/Routes.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Catalog from '../pages/Catalog';
-import Detail from '../pages/Detail';
+import Detail from '../pages/detail/Detail';
 
 const AppRoutes = () => {
     return (
